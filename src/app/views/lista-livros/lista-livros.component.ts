e9cb8aa11a728c9f3e98ab652fb38937655154ab
raul franck <raulengfranck@gmail.com>
2023-03-19 19:58:10 -0300

    throwError

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,6 @@
 import { Component, } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
+import { catchError,  debounceTime, distinctUntilChanged, filter, map, switchMap, tap, throwError } from 'rxjs';
 import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivrosService } from 'src/app/services/livros.service';
@@ -31,7 +31,10 @@ export class ListaLivrosComponent {
       switchMap((valorDigitado) =>
         this.livroService.buscar(valorDigitado)),
       tap(res => console.log(res)),
-      map((items) => this.livroResultadoParaLivros(items))
+      map((items) => this.livroResultadoParaLivros(items)),
+      catchError(erro => {
+        return throwError(() => new Error('Ocorreu um erro'))
+      })
     )
 
 }
